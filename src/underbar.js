148bf1325fa45553521c1a85fa7ba6ccd366752b
Max Kroshka <max.kroshka@gmail.com>
2015-11-26 14:15:49 -0800

    Implemented every and some methods

--- src/underbar.js
+++ src/underbar.js
@@ -202,13 +202,22 @@
 
   // Determine whether all of the elements match a truth test.
   _.every = function(collection, iterator) {
+  if(iterator === undefined){iterator = _.identity}
     // TIP: Try re-using reduce() here.
+    return _.reduce(collection, function(match,item){
+      if(!match){return false}
+      return !!iterator(item);
+    }, true);
   };
 
   // Determine whether any of the elements pass a truth test. If no iterator is
   // provided, provide a default one
   _.some = function(collection, iterator) {
     // TIP: There's a very clever way to re-use every() here.
+  if(iterator === undefined){iterator = _.identity}
+    return !(_.every(collection, function(item){
+        return !iterator(item);
+    }));
   };
 
 
