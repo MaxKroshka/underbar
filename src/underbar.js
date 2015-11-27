a707505014dd1141d08b42dfa1fd9a74fd4e37f5
Max Kroshka <max.kroshka@gmail.com>
2015-11-27 13:14:09 -0800

    Implemented _.delay method

--- src/underbar.js
+++ src/underbar.js
@@ -328,9 +328,12 @@
   // The arguments for the original function are passed after the wait
   // parameter. For example _.delay(someFunction, 500, 'a', 'b') will
   // call someFunction('a', 'b') after 500ms
-  _.delay = function(func, wait) {};
-
-
+  _.delay = function(func, wait) {
+    var args = Array.prototype.slice.call(arguments, 2);
+    return setTimeout(function() {
+      return func.apply(null, args);
+    }, wait);
+  };
   /**
    * ADVANCED COLLECTION OPERATIONS
    * ==============================
